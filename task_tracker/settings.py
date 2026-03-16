998769373db8ee07a7746b73d941d162cad07f4f
batman0102 <palachka01@gmail.com>
2026-03-16 20:33:24 +0600

    deployyyy1

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -119,10 +119,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
