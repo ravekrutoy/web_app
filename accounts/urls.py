18a1755f341629c501635c574994a115891e6250
batman0102 <palachka01@gmail.com>
2026-03-18 16:29:56 +0600

    logout_paggee

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from .views import SignupView, LoginView, LogoutView, HomeView
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='reg'),
-    path('login/', views.login_view, name='log'),
+    path('register/', views.register, name='reg'),
+    path('', views.login_view, name='log'),
     path('fail/', views.fail),
     path('home/', HomeView.as_view(), name='home'),
     path('api/auth/signup/', SignupView.as_view(), name="signup"),
